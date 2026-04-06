781cf2c492def25afa374ff1b805e00aca6a4936
che93283-rgb <che93283@gmail.com>
2026-04-07 01:54:28 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class WelcomeDevOps {
     public static void main(String[] args) {
         System.out.println("Welcome to DevOps day 2 session");
+        System.out.println("Welcome to DevOps day 3 session");
     }
 }
